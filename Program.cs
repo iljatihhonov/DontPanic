f0ecec06dcd9e1cc0c616eeb0d45b7c0ec5b6bd7
ilja.tihhonov <ilja.tihhonov@outlook.com>
2022-06-01 21:12:19 +0300

    Dont worry, stay calm

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DontPanic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Исходная строка
+            string str = "K33p C4lm";
+            //Проводим замену
+            str = str.Replace('3', 'e')?.Replace('4', 'a');
+            //Выводим результат
+            Console.WriteLine(str);
+        }
+    }
+}
